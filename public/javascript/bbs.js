427e20e13fc5ca4a1eaf49b129b7f29395ff8703
nobuyo <world_fantasy_2017@live.jp>
2016-11-24 19:21:20 +0900

    restrict delete key length

--- public/javascript/bbs.js
+++ public/javascript/bbs.js
@@ -8,3 +8,14 @@ $("input[name='mess']").change(function () {
     $("#btn-post").prop("disabled", true);
   }
 });
+
+
+function key_down(get_code){
+  if ((
+      get_code >= 48 && get_code <= 57
+   || get_code >= 96 && get_code <= 105
+  ) && ($("input[name='del_key']").val().length < 4) || (get_code == 8) && ($("input[name='del_key']").val().length < 5))
+   return true;
+  else
+   return false;
+}
