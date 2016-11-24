69021983c2db219059826ae0d6bb42e549527fa3
nobuyo <world_fantasy_2017@live.jp>
2016-11-24 14:36:00 +0900

    update bbs.rb

--- bbs.rb
+++ bbs.rb
@@ -16,10 +16,10 @@ end
 
 post '/' do
   username = Rack::Utils.escape_html(params[:user_name])
-  # if username.nil?
-  #   username = "NoName"
-  # end
-  p username
+  if username.empty?
+    username = "NoName"
+  end
+  # p username
   message  = Rack::Utils.escape_html(params[:mess])
   Post.create(message: message, posted_at: Time.now, user_name: username)
   redirect '/'
