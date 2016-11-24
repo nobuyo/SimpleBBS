b9262cd3031b5d724bbbecf8a0c05a4c6485e1e2
nobuyo <world_fantasy_2017@live.jp>
2016-11-24 15:38:45 +0900

    update public/javascript/bbs.js, etc.

--- public/javascript/bbs.js
+++ public/javascript/bbs.js
@@ -4,4 +4,7 @@ $("input[name='mess']").change(function () {
   if ($(this).val() != "") {
     $("#btn-post").prop("disabled", false);
   }
+  else {
+    $("#btn-post").prop("disabled", true);
+  }
 });
