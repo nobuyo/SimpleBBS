3eaaec558c8a8096e5c3567d69c4c34cb7d9ce87
nobuyo <world_fantasy_2017@live.jp>
2016-11-24 09:48:18 +0900

    add lacked line

--- bbs.rb
+++ bbs.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'active_record'
 
+set :environment, :production
+
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
 ActiveRecord::Base.establish_connection :development
 
@@ -8,8 +10,9 @@ class Post < ActiveRecord::Base
 end
 
 get '/' do
-  @posts = Post.all
-  erb :index
+#  @posts = Post.all
+#  erb :index
+  "Hello, world"
 end
 
 post '/' do
